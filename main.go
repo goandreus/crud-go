26eb8438695e4a47c458068ebbbfb5f48a6d6de3
goandreus <andreshtml5@gmail.com>
2019-10-24 16:30:14 -0500

    struck

--- main.go
+++ main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type Person struct {
+	ID        string   `json:"id.omitempty"`
+	FirstName string   `json:"firstname.omitempty"`
+	LastName  string   `json:"lastname.omitempty"`
+	Address   *Address `json:"address.omitempty"`
+}
+type Address struct {
+	City  string `json:"city.omitempty"`
+	State string `json:"state.omitempty"`
+}
+
+var people []Person
+
 func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}
+func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 
 }
 
 func main() {
 	router := mux.NewRouter()
 
+	people = append(people, Person{ID: "1", FirstName: "Andres", LastName: "Chavez", Address: &Address{City: "San Juan de Miraflores", State: "Lima"}})
+	people = append(people, Person{ID: "2", FirstName: "Carlos", LastName: "Chavez"})
+
 	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/pe ople{id}", GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/people{id}", CreatePersonEndpoint).Methods("GET")
+	router.HandleFunc("/people", DeletePersonEndpoint).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
