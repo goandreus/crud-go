dacf8ccd1138361b65bc79868adeab58390039cf
goandreus <andreshtml5@gmail.com>
2019-10-24 17:12:38 -0500

    postman

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 )
 
 type Person struct {
-	ID        string   `json:"id.omitempty"`
-	FirstName string   `json:"firstname.omitempty"`
-	LastName  string   `json:"lastname.omitempty"`
-	Address   *Address `json:"address.omitempty"`
+	ID        string   `json:"id,omitempty"`
+	FirstName string   `json:"firstname,omitempty"`
+	LastName  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
 }
 type Address struct {
-	City  string `json:"city.omitempty"`
-	State string `json:"state.omitempty"`
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
 }
 
 var people []Person
@@ -25,10 +25,22 @@ func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(people)
 }
 func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
-
+	params := mux.Vars(req)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
 }
 func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-
+	params := mux.Vars(req)
+	var person Person
+	_ = json.NewDecoder(req.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
 }
 func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 
